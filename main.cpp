f34e39f035b1fdeafb78a57266e6af597741d058
spl1t1kszzz <nik17403@gmail.com>
2022-09-15 14:08:58 +0700

    created factorial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
 
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else {
+        unsigned long long result = 1;
+        for (int i = 1; i <= n; ++i)
+            result *= i;
+        return result;
+    }
+}
 
-
+using namespace std;
 int main() {
+    cout<< factorial(6);
     return 0;
 }
