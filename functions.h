9740bbcdafc98db69ddb3c3233697e42fb699d26
spl1t1kszzz <nik17403@gmail.com>
2022-09-28 16:36:35 +0700

    working lab1

--- functions.h
+++ functions.h
@@ -1,10 +1,11 @@
 #ifndef PROGRAMTIMEMAINFRAME_FUNCTIONS_H
 #define PROGRAMTIMEMAINFRAME_FUNCTIONS_H
-
+#include <cmath>
 
 #include <sys/times.h>
 #include <unistd.h>
 
+using namespace std;
 long double factorial(int n) {
     if (n == 0 || n == 1)
         return 1;
@@ -22,13 +23,12 @@ long double exponenta(long double x, int n) {
         result += pow(x,i) / (long double)factorial(i);
     return result;
 }
-
 void clkGetTime(long double x, int n) {
     struct timespec start{}, end{};
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    exponenta(x, n);
+    std::cout << exponenta(x, n)<<endl;
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    printf("Time taken: %lf sec.\n", (double)end.tv_sec-(double)start.tv_sec+ 0.000000001*(double )(end.tv_nsec-start.tv_nsec));
+    printf("clock_gettime Time taken: %lf sec.\n", (double)end.tv_sec-(double)start.tv_sec+ 0.000000001*(double )(end.tv_nsec-start.tv_nsec));
 }
 
 
@@ -37,19 +37,11 @@ void getTimes(long double x, int n) {
     long cps = sysconf(_SC_CLK_TCK);
     unsigned long long clocks;
     times(&start);
-    exponenta(x, n);
+    std::cout << exponenta(x, n) << endl;
     times(&end);
     clocks = end.tms_utime - start.tms_utime;
-    printf("Time taken: %lf sec.\n", (double)clocks / (double )cps);
+    printf("times Time taken: %lf sec.\n", (double)clocks / (double )cps);
+
 }
 
-void rdtsc(long double x, int n){
-    uint64_t start;
-    uint64_t end;
-    uint64_t Current_Speed = 3200;
-    __asm__ __volatile__ ("mrs %0, cntvct_el0" : "=r" (start));
-    exponenta(x,n);
-    __asm__ __volatile__ ("mrs %0, cntvct_el0" : "=r" (end));
-    printf("%llu", (end - start) / Current_Speed);
-}
 #endif //PROGRAMTIMEMAINFRAME_FUNCTIONS_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-#include <cmath>
 #include "functions.h"
-#include <ctime>
-
+#include <cmath>
 using namespace std;
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc < 4)
+        throw std::invalid_argument("invalid number of arguments");
+    int timer = atoi(argv[3]);
+    long double x = (long double)(atoi(argv[1]));
+    int n = atoi(argv[2]);
+    switch (timer){
+        case 0:
+            cout<<exponenta(x,n)<<endl;
+            break;
+        case 1:
+            clkGetTime(x,n);
+            break;
+        case 2:
+            getTimes (x,n);
+            break;
+        default:
+            throw std::invalid_argument("invalid argument");
+            break;
+
+    }
     return 0;
 }
